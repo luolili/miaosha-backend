22c9dced130d6d12454c59173ff6cfcf6b3603ce
Administrator <2084267015@qq.com>
2019-06-27 21:58:29 +0800

    fix

--- src/main/java/com/miaosha/error/EmBusinessError.java
+++ src/main/java/com/miaosha/error/EmBusinessError.java
@@ -4,10 +4,12 @@ public enum EmBusinessError implements CommonError{
     //以20000开头的信息为用户信息错误
     USER_NOT_EXIST(10001, "用户不存在"),
     UNKNOWN_ERROR(10002, "未知错误"),
+    
     //通用错误码
     PARAMETER_VALIDATION_ERROR(200001, "参数不合法"),
     USER_LOGIN_FAIL(200002, "用户手机号或密码不正确"),
     USER_NOT_LOGIN(200003, "用户还未登陆"),
+
     //3000开头是交易信息错误
     STOCK_NOT_ENOUGH(300001, "库存不足"),
     ;
